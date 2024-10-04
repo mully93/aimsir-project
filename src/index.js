b45cebe932898be60f5dc6cae9ed437972834dd8
mully93 <emmamullane93@gmail.com>
2024-10-04 14:55:06 +0100

    updated forecast js

--- src/index.js
+++ src/index.js
@@ -54,8 +54,34 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Mon", "Tue", "Wed", "Thur", "Fri"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+<div class="weather-forecast-day">
+  <div class="weather-forecast-date">${day}</div>
+      <div class="weather-forecast-icon">🌤️</div>
+      <div class="weather-forecast-temperatures">
+        <div class="weather-forecast-temperature">
+          <strong>15°</strong></div>
+            <div class="weather-forecast-temperature">9°</div>
+    </div>
+</div>
+`;
+  });
+
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 console.log(searchFormElement);
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Limerick");
+displayForecast();
